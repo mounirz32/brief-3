053ac6fcae5ba0226dd4f464011f7b21f9413cc1
mounirz32 <77145338+mounirz32@users.noreply.github.com>
2021-02-23 12:18:23 +0100

    second-commit

--- brief-3/js/script.js
+++ brief-3/js/script.js
@@ -1,32 +1,32 @@
-var boitDeVitesse = 0;
-var carbu = 0;
-var prix = 0;
+var boitDeVitesse = {"automatique": 0.19, "manuelle": 0};
+var carbu = {"essence": 0.14, "electrique": 0.05, "hybrid": 0.09, "diesel": 0.21};
+var prixv = {"Citadine":12,"Compact":14,"Berline":20,"Utilitaire":16,"Moto":10,"Camion":250,"Chantie":900};
 
 var Citadine=[
 	["boit-vitesse-2","none"],
-	["hyb", "visible"],
-	["die", "visible"],
-	["elect", "visible"],
-	["Esse", "visible"],
-	["boit-vitesse-1", "visible"]
+	["hyb", "flex"],
+	["die", "flex"],
+	["elect", "flex"],
+	["Esse", "flex"],
+	["boit-vitesse-1", "flex"]
 ];
 
 var Compact=[
 	["boit-vitesse-2", "none"],
 	["elect", "none"],
-	["hyb", "visible"],
-	["Esse", "visible"],
-	["die", "visible"],
-	["boit-vitesse-1", "visible"]
+	["hyb", "flex"],
+	["Esse", "flex"],
+	["die", "flex"],
+	["boit-vitesse-1", "flex"]
 ];
 
 var Berline=[
 	["elect", "none"],
 	["boit-vitesse-1", "none"],
-	["hyb", "visible"],
-	["Esse", "visible"],
-	["die", "visible"],
-	["boit-vitesse-2", "visible"]
+	["hyb", "flex"],
+	["Esse", "flex"],
+	["die", "flex"],
+	["boit-vitesse-2", "flex"]
 ];
 
 var Utilitaire=[
@@ -34,33 +34,94 @@ var Utilitaire=[
 	["hyb", "none"],
 	["Esse", "none"],
 	["boit-vitesse-2", "none"],
-	["die", "visible"],
-	["boit-vitesse-1", "visible"]
+	["die", "flex"],
+	["boit-vitesse-1", "flex"]
 ];
 
-function TypeCitadine() {
+var Moto=[
+	["elect", "flex"],
+	["Esse", "flex"],
+	["hyb", "none"],
+	["boit-vitesse-2", "none"],
+	["die", "none"],
+	["boit-vitesse-1", "none"]
+];
 
-	for( var i = 0; Citadine.length > i; i++) {
-		
+var Camion=[
+	["elect", "none"],
+	["Esse", "none"],
+	["hyb", "none"],
+	["boit-vitesse-1", "none"],
+	["boit-vitesse-2", "flex"],
+	["die", "flex"]
+];
+
+var Chantie=[
+	["elect", "none"],
+	["Esse", "flex"],
+	["hyb", "none"],
+	["boit-vitesse-1", "flex"],
+	["boit-vitesse-2", "none"],
+	["die", "flex"]
+];
+
+
+
+
+function TypeCitadine() {
+	index = "Citadine";
+
+	for( var i = 0;i< Citadine.length  ; i++) {
 		document.getElementById(Citadine[i][0]).style.display = Citadine[i][1];
 	}
 };
 
 
 function TypeCompact() {
-	for( var j = 0; Compact.length > j; j++) {
+	index = "Compact";
+	for( var j = 0; j<Compact.length ; j++) {
 		document.getElementById(Compact[j][0]).style.display = Compact[j][1];
 	}
 };
 
 function TypeBerline() {
-	for( var a = 0; Berline.length > a; a++) {
+	index = "Berline";
+	for( var a = 0; a < Berline.length ; a++) {
 		document.getElementById(Berline[a][0]).style.display = Berline[a][1];
 	}
 };
 
 function TypeUtilitaire() {
-	for( var x = 0; Utilitaire.length > x; x++) {
+	index = "Utilitaire";
+	for( var x = 0;x< Utilitaire.length ; x++) {
 		document.getElementById(Utilitaire[x][0]).style.display = Utilitaire[x][1];
 	}
 };
+
+function TypeMoto() {
+	index = "Moto";
+	for( var i = 0;i< Moto.length  ; i++) {
+		document.getElementById(Moto[i][0]).style.display = Moto[i][1];
+	}
+};
+
+function TypeCamion() {
+	index = "Camion";
+	for( var i = 0;i< Camion.length  ; i++) {
+		document.getElementById(Camion[i][0]).style.display = Camion[i][1];
+	}
+};
+
+function TypeChantie() {
+	index = "Chantie";
+	for( var i = 0;i< Chantie.length  ; i++) {
+		document.getElementById(Camion[i][0]).style.display = Chantie[i][1];
+	}
+};
+
+
+
+
+
+
+
